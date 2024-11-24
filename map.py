d707bcc3f73ad5aff1d1ebd1e8ad22eb29a94219
Dhiren Narne <narnedhiren@gmail.com>
2024-11-23 22:12:40 -0500

    background image added

--- map.py
+++ map.py
@@ -1,19 +1,17 @@
 from cmu_graphics import *
 
-
 def onAppStart(app):
     app.width = 750
     app.height = 500
-    app.url = 'https://t4.ftcdn.net/jpg/02/82/18/79/360_F_282187946_WwV8GHXwGB9x5j6OqEoMG6emNIMUBDxY.jpg'
+    
+    # Path to the background image
+    app.backgroundPath = '/Users/dhirennarne/Desktop/Src/Images/background.jpeg'
 
 def redrawAll(app):
-    imageWidth, imageHeight = app.width, app.height
-
-    drawLabel(f'Original ({imageWidth}x{imageHeight})', 125, 75, size=16)
-    drawImage(app.url, 125, 200, align='center')
-
+    # Draw the background image covering the entire canvas
+    drawImage(app.backgroundPath, 0, 0, width=app.width, height=app.height)
 
 def main():
     runApp(width=750, height=500)
-    
+
 main()
